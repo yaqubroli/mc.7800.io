5fe46c3c1f4a641dc693ebe6b3349c4f6095ed28
yaqubroli <walchuk2018@icloud.com>
2023-05-14 22:37:56 -0700

    made compatible w mc-mailer

--- src/components/WhitelistUI.js
+++ src/components/WhitelistUI.js
@@ -7,21 +7,21 @@ function WhitelistUI() {
             <h1>Join the whitelist</h1>
             <div id="with-sta-email">
                 <h3>If you have a St. Andrews email</h3>
-            <form action="https://mc.7800.io/whitelist" method="post">
-                <input type="hidden" name="st-andrews" value="true" />
+            <form action="http://localhost:8080/send-sta" method="post">
                 <label htmlFor="email">St. Andrews email (prefix only):</label>
                 <input type="text" id="email" name="email" />
-                <label htmlFor="minecraft-username">Minecraft username:</label>
-                <input type="text" name="minecraft-username" />
+                <label htmlFor="minecraft_username">Minecraft username:</label>
+                <input type="text" name="minecraft_username" />
                 <input type="submit" value="Submit" />
             </form>
             </div>
             <div id="without-sta-email">
             <h3>If you don't</h3>
-            <form action="https://mc.7800.io/whitelist" method="post">
-                <input type="hidden" name="st-andrews" value="false" />
-                <label htmlFor="minecraft-username">Minecraft username:</label>
-                <input type="text" name="minecraft-username" />
+            <form action="http://localhost:8080/send-written" method="post">
+                <label htmlFor="minecraft_username">Minecraft username:</label>
+                <input type="text" name="minecraft_username" />
+                <label htmlFor="email">Email (full; prefix@domain):</label>
+                <input type="text" name="email" />
                 <label htmlFor="reason">Reason for joining:</label>
                 <textarea name="reason" id="reason" cols="30" rows="10"></textarea>
                 <input type="submit" value="Submit" />
